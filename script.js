a4eddef434a6945aadaebc4d5acb25bacbf50000
J0J0 <iditaroddream@gmail.com>
2022-12-06 21:09:00 -0600

    update

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var description = document.querySelector('#description')
 var temperature = document.querySelector('#temperature')
 var wind = document.querySelector('#wind')
 var humidity = document.querySelector('#humidity')
+var temp = document.querySelector('#temperature')
+var fiveDaySection = document.querySelector(".five-day-forcast")
 
 function getcityweather(city) {
     console.log(`${Weather_API_URL}/data/2.5/weather?q=${city}&units=imperial&appid=${API_Key}`)
@@ -19,17 +21,17 @@ function getcityweather(city) {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            temp.innerText = "Temperature: " + data.main.temp + "F";
+            humidity.innerText = "Humidity: " + data.main.humidity + "%";
+            wind.innerText = "Wind Speed: " + data.wind.speed + "MPH";
 
-            temp.text("Temperature: " + data.current.temp + "F");
-            humidity.text("Humidity: " + data.current.humidity + "%");
-            wind.text("Wind Speed: " + data.current.wind_speed + "MPH");
-
+            console.log(dayjs(new Date()).format('MM/DD/YYYY'))
             forcastTitle.innerHTML = `City: ${data.name}`
             date.innerHTML = new Date().toLocaleDateString()
             description.innerHTML = `Description: ${data.weather[0].description}`
-            temperature.innerHTML = `temperature: ${data.list.main.temp}`
-            wind.innerHTML = `wind: ${data.list.wind.speed}`
-            humidity.innerHTML = `humidity: ${data.list.main.humidity}`
+            temperature.innerHTML = `temperature: ${data.main.temp}`
+            wind.innerHTML = `wind: ${data.wind.speed}`
+            humidity.innerHTML = `humidity: ${data.main.humidity}`
         })
 }
 
@@ -38,23 +40,27 @@ function get5day(city) {
     fetch(`${Weather_API_URL}/data/2.5/forecast?q=${city}&units=imperial&limit=5&appid=${API_Key}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.list)
-
+            console.log(data)
             var results = data.list
-
-            for (let i = 0; i < results.length; i += 8)
-                console.log(data)
-
-        }
-        )
+            var section = document.createElement("div")
+            var day = 1
+            for (let i = 0; i < results.length; i += 8) {
+                console.log(results[i].main.temp)
+                var text = `
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    // console.log(dayjs(new Date()).format('MM/DD/YYYY'))//
+                        <h5 class="card-title">Day ${day++}</h5>
+                        <p class="Temp">${results[i].main.temp}F</p>
+                        <p class="Wind">${results[i].wind.speed}MPH</p>
+                        <p class="humidity">${results[i].main.humidity}%</p>
+                    </div>
+                </div>`
+                section.innerHTML += text
+            }
+            fiveDaySection.append(section)
+        })
 };
-date.innerHTML = new Date().toLocaleDateString()
-description.innerHTML = `Description: ${data.weather[0].id.main.description}`
-temperature.innerHTML = `temperature: ${data.main.temp}`
-wind.innerHTML = `wind: ${data.wind.speed}`
-humidity.innerHTML = `humidity: ${data.main.humidity}`
-console.log(data)
-forcastTitle.innerHTML = `City: ${list.arrray[40]}`
 
 cityFormEl.addEventListener('submit', event => {
     event.preventDefault();
@@ -63,7 +69,6 @@ cityFormEl.addEventListener('submit', event => {
     if (city) {
         getcityweather(city);
     }
-
 })
 
 cityFormEl.addEventListener('submit', event => {
